0cc65ca2d7eb6d836a5a1e8724ab7b81ab28de23
fabriciofurni <58267210+fabriciofurni@users.noreply.github.com>
2023-06-06 17:10:59 +0200

    Update rfdistance.r

--- rfdistance.r
+++ rfdistance.r
@@ -3,6 +3,19 @@
 library(ape)
 library(TreeDist)
 
+##### creating list of tree files from the 1Mbp Loci trees ##
+
+listcsv <- dir(pattern = "*.raxml.support")
+
+listcsv <- read.csv("list_files.txt")
+
+write.csv(listcsv,file= "list_files.txt")
+
+## openning files as trees using lapply and ape  ##
+
+t <- read.csv("list_files.txt", header = FALSE,sep = ",")
+
+list <- lapply(t$V1,read.tree)
 
 
 ## load whole autosome tree ###
